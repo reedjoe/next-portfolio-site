98323869b4ec4a10900034199e65e0bddcf5eca0
Joe Reed <reedjoe36@gmail.com>
2023-02-02 20:02:45 +0000

    Add intial code for Footer component

---
+++ components/layout/footer.tsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/Layout.module.css';
+
+export default function Footer() {
+  return (
+    <>
+      <div className={styles.footerContainer}>
+        <h1>Placeholder</h1>
+        <h2>Footer</h2>
+      </div>
+    </>
+  )
+};
--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../../styles/Layout.module.css';
+import Footer from './footer';
 import { LayoutProps } from './layout.interface';
 import Navbar from "./navbar";
 
@@ -9,12 +10,13 @@ const Layout = ({ children, pageTitle }: LayoutProps) => (
       <title>{pageTitle}</title>
       <meta name="description" content="Portfolio site description" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="./favicon.ico" />
     </Head>
     <Navbar />
     <div className={styles.mainContentContainer}>
       {children}
     </div>
+    <Footer />
   </>
 );
 export default Layout;
