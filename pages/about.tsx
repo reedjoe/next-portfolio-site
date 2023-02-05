2d15dba372457ded1ebe7068c2f3236437af364e
Joe Reed <reedjoe36@gmail.com>
2023-02-05 22:00:58 +0000

    Add initial content for about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,23 @@
-import Layout from '../components/layout/layout'
+import Layout from '../components/layout/layout';
+import styles from '../styles/About.module.css';
 
 export default function About() {
   return (
     <>
       <Layout pageTitle='Portfolio - About'>
-        <div>About page working</div>
+        {/* <div>About page working</div> */}
+        <div className={styles.container}>
+          <div className={styles.leftContainer}>
+            <h3>Bio</h3>
+            <h3>Education</h3>
+            <h3>Employment</h3>
+          </div>
+          <div className={styles.rightContainer}>
+            <div>Here is the Bio. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
+            <div>Here is the Education. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
+            <div>Here is the Employment. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
+          </div>
+        </div>
       </Layout>
     </>
   )
