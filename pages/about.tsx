208038ccd0b53b56be61d64c75853762efbaa602
Joe Reed <reedjoe36@gmail.com>
2023-02-06 22:40:02 +0000

    Add rough code for animating height of About sections

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,7 +5,6 @@ export default function About() {
   return (
     <>
       <Layout pageTitle='Portfolio - About'>
-        {/* <div>About page working</div> */}
         <div className={styles.container}>
           <div className={styles.leftContainer}>
             <h3>Bio</h3>
@@ -13,9 +12,32 @@ export default function About() {
             <h3>Employment</h3>
           </div>
           <div className={styles.rightContainer}>
-            <div>Here is the Bio. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
-            <div>Here is the Education. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
-            <div>Here is the Employment. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
+            <div className={styles.sectionWrapper}>
+              <div className={styles.sectionContainer}>
+                Here is the Bio. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                <br/>
+                <br/>
+                Here is the Employment. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              </div>
+            </div>
+            <div className={styles.sectionWrapper}>
+              <div className={styles.sectionContainer}>
+                <h3>Degree name</h3>
+                <br/>
+                <h4>Universtity name - Grade</h4>
+                <br/>
+                <p>Here are some details of the degree. Some of the modules studied include placeholder1, placeholder2 and placeholder3.</p>
+                <p>The final year project was on blank and involved such and such.</p>
+              </div>
+            </div>
+            <div className={styles.sectionWrapper}>
+              <div className={styles.sectionContainer}>
+                Here is the Employment. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                <br/>
+                <br/>
+                Here is the Employment. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+              </div>
+            </div>
           </div>
         </div>
       </Layout>
