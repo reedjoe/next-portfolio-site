f8447f8a73653a30064716052d6412bab66d0705
Joe Reed <reedjoe36@gmail.com>
2023-02-12 23:09:27 +0000

    Update About sections to rotate on click

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,33 +1,83 @@
+import { LegacyRef, useRef, useState } from 'react';
 import Layout from '../components/layout/layout';
 import styles from '../styles/About.module.css';
 
 export default function About() {
+  const rightContainerRef = useRef<HTMLElement>(null);
+  const [currentSection, setCurrentSection] = useState('bio');
+  const [currentRotation, setCurrentRotation] = useState(0);
+  const baseTransform = 'perspective(2000px) rotateX(-90deg) rotate(180deg) scaleX(-1)';
+  
+  const setSection = (newSection: string) => {
+    if (!rightContainerRef.current) {
+      return;
+    }
+
+    let newRotation = currentRotation;
+    switch (newSection) {
+      case 'bio':
+        switch (currentSection) {
+          case 'education':
+            newRotation = currentRotation + 120;
+            break;
+          case 'employment':
+            newRotation = currentRotation - 120;
+            break;
+        }
+        break;
+      case 'education':
+        switch (currentSection) {
+          case 'bio':
+            newRotation = currentRotation - 120;
+            break;
+          case 'employment':
+            newRotation = currentRotation + 120;
+            break;
+        } 
+        break;
+      case 'employment':
+        switch (currentSection) {
+          case 'bio':
+            newRotation = currentRotation + 120;
+            break;
+          case 'education':
+            newRotation = currentRotation - 120;
+            break;
+        }
+        break;
+    }    
+
+    setCurrentSection(newSection);
+    setCurrentRotation(newRotation);
+    rightContainerRef.current.style.transform = `${baseTransform} rotate(${newRotation}deg)`;
+  }
+  
   return (
     <>
       <Layout pageTitle='Portfolio - About'>
         <div className={styles.container}>
           <div className={styles.leftContainer}>
             {/* TODO: look at https://codepen.io/nikolamitic/pen/vpNoNq */}
-            <div className={styles.sectionLink}>
+            <div className={styles.sectionLink} onClick={() => setSection('bio')}>
               <span className={styles.sectionLinkStatic}>Bio</span>
               <div className={styles.sectionLinkDynamic}>
                 <span className={styles.sectionLinkDynamicInner}>Bio</span>
               </div>
             </div>
-            <div className={styles.sectionLink}>
-              <span className={styles.sectionLinkStatic}>Education</span>
-              <div className={styles.sectionLinkDynamic}>
-                <span className={styles.sectionLinkDynamicInner}>Education</span>
-              </div>
-            </div>
-            <div className={styles.sectionLink}>
+            <div className={styles.sectionLink} onClick={() => setSection('employment')}>
               <span className={styles.sectionLinkStatic}>Employment</span>
               <div className={styles.sectionLinkDynamic}>
                 <span className={styles.sectionLinkDynamicInner}>Employment</span>
               </div>
             </div>
+            <div className={styles.sectionLink} onClick={() => setSection('education')}>
+              <span className={styles.sectionLinkStatic}>Education</span>
+              <div className={styles.sectionLinkDynamic}>
+                <span className={styles.sectionLinkDynamicInner}>Education</span>
+              </div>
+            </div>
           </div>
-          <div className={styles.rightContainer}>
+          <div ref={rightContainerRef as LegacyRef<HTMLDivElement>} className={`${styles.rightContainer}`}>
             <div className={styles.sectionWrapper}>
               <div className={styles.sectionContainer}>
                 Here is the Bio. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
